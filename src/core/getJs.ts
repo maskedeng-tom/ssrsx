d1aa2710da9f0da27e1509fb14466b078aa848de
maskedeng-tom <maskedeng.tom@gmail.com>
2024-02-18 16:08:12 +0900

    fixed client script load path

--- src/core/getJs.ts
+++ src/core/getJs.ts
@@ -4,12 +4,12 @@ import path from 'path';
 ////////////////////////////////////////////////////////////////////////////////
 
 const getRequireJs = (): string => {
-  const requireJs = fs.readFileSync(path.join(__dirname, '../client', 'require.js'), 'utf-8');
+  const requireJs = fs.readFileSync(path.join(__dirname, '../../client', 'require.js'), 'utf-8');
   return requireJs;
 };
 
 const getLoadEventsJs = (): string => {
-  const loadEventsJs = fs.readFileSync(path.join(__dirname, '../client', 'loadEvents.js'), 'utf-8');
+  const loadEventsJs = fs.readFileSync(path.join(__dirname, '../../client', 'loadEvents.js'), 'utf-8');
   return loadEventsJs;
 };
 
