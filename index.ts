e4d3fe97b323fc222969f6e2565ed819360ef406
maskedeng-tom <maskedeng.tom@gmail.com>
2024-02-19 17:58:47 +0900

    d.ts test

--- index.ts
+++ index.ts
@@ -1,38 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-namespace
-export namespace JSX {
-
-  // 仮想Element定義
-  export type Element = import('./src/types/types').VirtualElement;
-
-  // スタイル引数
-  export type StyleProps = import('./src/types/types').StyleProps;
-
-  // 仮想子ノード
-  export type Children = import('./src/types/types').VirtualChildren;
-
-  // エレメントの子ノードの定義 => children
-  export interface ElementChildrenAttribute {
-    children: Children;
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  // event for all
-  export interface lIntrinsicElementsProps {
-    style?: string | StyleProps | {[key:string] : unknown};
-    className?: string | string[] | {[name: string]: boolean};
-    [key:string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-
-  // エレメントの定義
-  export interface IntrinsicElements {
-    [elementName: string]: lIntrinsicElementsProps;
-  }
-
-}
-
 import ssrsx, { SsrsxOptions } from './src/';
 import { initializeStyles, setStyle, setScopedStyle, getStyles, styleToString } from './src/core/cssSupport';
 //
